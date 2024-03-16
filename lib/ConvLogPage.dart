cae20ea2bf86b2c03819bd3a7373acbf6a819664
Nikhil-1426 <parkar.nikhil1426@gmail.com>
2024-03-17 03:52:21 +0530

    Third commit

--- lib/ConvLogPage.dart
+++ lib/ConvLogPage.dart
@@ -1,21 +1,98 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:io';
 
-class ConvLogPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class ConvLogPage extends StatefulWidget {
+  @override
+  _ConvLogPageState createState() => _ConvLogPageState();
+}
+
+class _ConvLogPageState extends State<ConvLogPage> {
+  FlutterSoundRecorder? _audioRecorder = FlutterSoundRecorder();
+  String? _recordingPath;
+  bool _isRecording = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _audioRecorder!.openAudioSession().then((value) {});
+  }
+
+  @override
+  void dispose() {
+    _audioRecorder!.closeAudioSession();
+    super.dispose();
+  }
+
+  Future<void> _startRecording() async {
+    setState(() {
+      _isRecording = true;
+    });
+
+    // Start recording
+    await _audioRecorder!.startRecorder(toFile: 'tempAudio');
+
+    // Start a timer to stop recording after one minute
+    Timer(Duration(minutes: 1), () async {
+      await _stopRecording();
+    });
+  }
+
+  Future<void> _stopRecording() async {
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    await _audioRecorder!.stopRecorder();
+    setState(() {
+      _isRecording = false;
+    });
+    String filePath = '${appDocDir.path}/${_recordingPath}.aac';
+    File file = File(filePath);
+    if (file.existsSync()) {
+      try {
+        final Reference storageReference =
+            FirebaseStorage.instance.ref('https://console.firebase.google.com/u/0/project/remembron/storage/remembron.appspot.com/files/~2Faudio_files').child('audio_files/${DateTime.now()}.aac');
+        final UploadTask uploadTask = storageReference.putFile(file);
+        // Properly handle the completion of the uploadTask
+        await uploadTask;
+        print('File uploaded successfully');
+      } catch (e) {
+        print('Error uploading file: $e');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Conversation Log"),
+        title: Text('Conv Log Page'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Conversation Log Page Content"),
-            // Add your specific conversation log content here
+            if (_isRecording) ...[
+              Text('Recording...'),
+              ElevatedButton(
+                onPressed: _stopRecording,
+                child: Text('Stop Recording'),
+              ),
+            ] else ...[
+              ElevatedButton(
+                onPressed: _startRecording,
+                child: Text('Start Recording'),
+              ),
+            ],
           ],
         ),
       ),
     );
   }
 }
+
+
+
+
--- lib/RecognizePage.dart
+++ lib/RecognizePage.dart
@@ -75,13 +75,14 @@
 //   }
 // }
 
-
-
+import 'dart:async'; // Import async library for Timer
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
+import 'package:video_player/video_player.dart';
 
 class RecognizePage extends StatefulWidget {
   @override
@@ -90,6 +91,8 @@ class RecognizePage extends StatefulWidget {
 
 class _RecognizePageState extends State<RecognizePage> {
   File? _image;
+  File? _video;
+  late Timer _videoTimer;
 
   Future<void> _pickImage() async {
     final pickedImage =
@@ -110,6 +113,46 @@ class _RecognizePageState extends State<RecognizePage> {
       });
     }
   }
+
+  Future<void> _takeVideo() async {
+  final pickedVideo = await ImagePicker().pickVideo(source: ImageSource.camera);
+  if (pickedVideo != null) {
+    setState(() {
+      _video = File(pickedVideo.path);
+    });
+
+    _videoTimer = Timer(Duration(seconds: 9), () {
+      setState(() {
+        _video = null; // Stop video recording after 9 seconds
+      });
+    });
+
+    print('Video recording started.');
+    print('Current time: ${DateTime.now()}');
+
+    // Check video format (Android only)
+    if (Platform.isAndroid) {
+      final videoPlayerController = VideoPlayerController.file(_video!);
+      await videoPlayerController.initialize();
+
+      // Check if the video has a size
+      final size = videoPlayerController.value.size;
+      if (size == null) {
+        print('Warning: Unable to load video.');
+        // Show user a message about the error
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Failed to load video. Please try again.'),
+          ),
+        );
+      } else {
+        print('Video loaded successfully.');
+      }
+
+      videoPlayerController.dispose(); // Dispose the controller after use
+    }
+  }
+}
 
   Future<void> _uploadImage() async {
     if (_image == null) {
@@ -136,36 +179,95 @@ class _RecognizePageState extends State<RecognizePage> {
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Recognize Page'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _image == null ? Text('No image selected.') : Image.file(_image!),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: _takePicture,
-              child: Text('Take Picture'),
-            ),
-            SizedBox(height: 5),
-            ElevatedButton(
-              onPressed: _pickImage,
-              child: Text('Select Image'),
-            ),
-            SizedBox(height: 5),
-            ElevatedButton(
-              onPressed: _uploadImage,
-              child: Text('Upload Image'),
-            ),
-          ],
-        ),
-      ),
-    );
+  Future<void> _uploadVideo() async {
+  if (_video == null) {
+    return;
+  }
+
+  String url = 'http://172.16.41.231:3000/uploadVideo';
+  var request = http.MultipartRequest('POST', Uri.parse(url));
+
+  // Add the video file with content type
+  request.files.add(await http.MultipartFile.fromPath(
+    'video',
+    _video!.path,
+    contentType: MediaType('video', 'mp4'), // Specify the content type
+  ));
+
+  var response = await request.send();
+
+  if (response.statusCode == 200) {
+    print('Video uploaded successfully!');
+  } else {
+    print('Failed to upload video. Status code: ${response.statusCode}');
   }
 }
 
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('Recognize Page'),
+    ),
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _image == null && _video == null
+              ? Text('No media selected', style: TextStyle(fontSize: 16)) // Increased text size to 16
+              : _image != null
+                  ? Image.file(_image!)
+                  : Text('Video selected.', style: TextStyle(fontSize: 13)),
+          SizedBox(height: 15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: _takePicture,
+                child: Text('Recognize', style: TextStyle(fontSize: 13)),
+                style: ButtonStyle(
+                  minimumSize: MaterialStateProperty.all<Size>(Size(120, 30)), // Adjust button size here
+                ),
+              ),
+              ElevatedButton(
+                onPressed: _uploadImage,
+                child: Text('Upload Image', style: TextStyle(fontSize: 13)),
+                style: ButtonStyle(
+                  minimumSize: MaterialStateProperty.all<Size>(Size(120, 30)), // Adjust button size here
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 5),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: _takeVideo,
+                child: Text('Add a Face', style: TextStyle(fontSize: 13)),
+                style: ButtonStyle(
+                  minimumSize: MaterialStateProperty.all<Size>(Size(120, 30)), // Adjust button size here
+                ),
+              ),
+              ElevatedButton(
+                onPressed: _uploadVideo,
+                child: Text('Update Video', style: TextStyle(fontSize: 13)),
+                style: ButtonStyle(
+                  minimumSize: MaterialStateProperty.all<Size>(Size(120, 30)), // Adjust button size here
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+  @override
+  void dispose() {
+    // Cancel the timer when the widget is disposed
+    _videoTimer.cancel();
+    super.dispose();
+  }
+}
